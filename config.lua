7567dbbc95b0f730efdcbf53dcab2cbdb11b9923
FkinGuy <jarod@faltine.com>
2023-10-05 09:34:13 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -27,18 +27,21 @@ Config.canReceiveWeapons = true  -- true | false
 Config.receiveWeapon = 10        -- If below this number, then can receive
 Config.chanceGettingWeapon = 100 -- Percentage out of 100
 Config.weapons = {               -- You will receive a random weapon from the list
-    { name = "WEAPON_REVOLVER_CATTLEMAN", label = "Cattleman Revolver" },
-    { name = "WEAPON_REPEATER_CARBINE",   label = "Carbine Repeater" },
-    { name = "WEAPON_RIFLE_VARMINT",      label = "Varmint Rifle" }
+    { name = "WEAPON_REVOLVER_CATTLEMAN", label = WEAPON_REVOLVER_CATTLEMAN_label },
+    { name = "WEAPON_REPEATER_CARBINE",   label = WEAPON_REPEATER_CARBINE_label },
+    { name = "WEAPON_RIFLE_VARMINT",      label = WEAPON_RIFLE_VARMINT_label }
 }
 
 -- Translations
 Config.Translate = {
     invFullWeapon = "You can't carry any more WEAPONS",
-    invFullItems = "You can't carry any more ITEMS",
+    invFullItems = "You can't carry any more ",
     youGot = "You got",
     nugget = "nugget",
     water = "Water",
     ammorepeaternormal = "Normal Ammo Repeater",
-    ammoriflenormal = "Normal Ammo Rifle"
+    ammoriflenormal = "Normal Ammo Rifle",
+    WEAPON_REVOLVER_CATTLEMAN_label = "Cattleman Revolver",
+    WEAPON_REPEATER_CARBINE_label = "Carbine Repeater",
+    WEAPON_RIFLE_VARMINT_label = "Varmint Rifle"
 }  # isn't there a table somewhere, where I could just pull the translation of items ?
