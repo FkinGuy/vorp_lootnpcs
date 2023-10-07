66525b77883fd34b676e4f6893eb30d34372c5c3
FkinGuy <jarod@faltine.com>
2023-10-07 23:31:58 +0200

    Update languages.lua

--- languages.lua
+++ languages.lua
@@ -1,38 +1,6 @@
 Language = "Francais"
+# isn't there a way to get the language set in redm client ?
 
-Config = {}
-
--- Enable or disable the chance to receive ITEMS
-Config.canReceiveItems = false -- true | false
-Config.receiveItem = 20        -- If below this number, then can receive
-Config.chanceGettingItem = 100 -- Percentage out of 100
-Config.items = {               -- You will receive a random item from the list
-    { name = "Water",              label = Config.Translate.water,                amount = 1 },
-    { name = "ammorepeaternormal", label = Config.Translate.ammorepeaternormal, amount = 1 },
-    { name = "ammoriflenormal",    label = Config.Translate.ammoriflenormal,    amount = 1 },
-}
-
--- Enable or disable the chance to receive MONEY
-Config.canReceiveMoney = false  -- true | false
-Config.receiveMoney = 50        -- If below this number, then can receive
-Config.chanceGettingMoney = 100 -- Percentage out of 100
-Config.money = { 0.5, 1, 1.5 }  -- You will receive a random value from the list
-
--- Enable or disable the chance to receive GOLD
-Config.canReceiveGold = false -- true | false
-Config.receiveGold = 5        -- If below this number, then can receive
-Config.chanceGettingGold = 10 -- Percentage out of 100
-Config.gold = { 1, 2, 3 }     -- You will receive a random value from the list
-
--- Enable or disable the chance to receive WEAPONS
-Config.canReceiveWeapons = true  -- true | false
-Config.receiveWeapon = 10        -- If below this number, then can receive
-Config.chanceGettingWeapon = 100 -- Percentage out of 100
-Config.weapons = {               -- You will receive a random weapon from the list
-    { name = "WEAPON_REVOLVER_CATTLEMAN", label = Config.Translate.WEAPON_REVOLVER_CATTLEMAN_label },
-    { name = "WEAPON_REPEATER_CARBINE",   label = Config.Translate.WEAPON_REPEATER_CARBINE_label },
-    { name = "WEAPON_RIFLE_VARMINT",      label = Config.Translate.WEAPON_RIFLE_VARMINT_label }
-}
 
 -- Translations
 if Language == "English" then
@@ -60,5 +28,5 @@ elseif Language == "Francais" then
         WEAPON_REVOLVER_CATTLEMAN_label = "un révolver Cattleman",
         WEAPON_REPEATER_CARBINE_label = "une carabine à répétition",
         WEAPON_RIFLE_VARMINT_label = "un fusil à petit gibier"
-    }  # isn't there a table somewhere, where I could just pull the translation of items ?
+    }  
 end
