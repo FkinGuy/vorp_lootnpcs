eebc7972589364c7592d38530eaeecf71f58f7c8
FkinGuy <jarod@faltine.com>
2023-10-07 23:23:27 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
+Language = "Francais"
+
 Config = {}
 
 -- Enable or disable the chance to receive ITEMS
@@ -33,15 +35,30 @@ Config.weapons = {               -- You will receive a random weapon from the li
 }
 
 -- Translations
-Config.Translate = {
-    invFullWeapon = "You can't carry any more WEAPONS",
-    invFullItems = "You can't carry any more ",
-    youGot = "You got",
-    nugget = "nugget",
-    water = "Water",
-    ammorepeaternormal = "Normal Ammo Repeater",
-    ammoriflenormal = "Normal Ammo Rifle",
-    WEAPON_REVOLVER_CATTLEMAN_label = "Cattleman Revolver",
-    WEAPON_REPEATER_CARBINE_label = "Carbine Repeater",
-    WEAPON_RIFLE_VARMINT_label = "Varmint Rifle"
-}  # isn't there a table somewhere, where I could just pull the translation of items ?
+if Language == "English" then
+    Config.Translate = {
+        invFullWeapon = "You can't carry any more WEAPONS",
+        invFullItems = "You can't carry any more ",
+        youGot = "You got",
+        nugget = "nugget",
+        water = "Water",
+        ammorepeaternormal = "Normal Ammo Repeater",
+        ammoriflenormal = "Normal Ammo Rifle",
+        WEAPON_REVOLVER_CATTLEMAN_label = "Cattleman Revolver",
+        WEAPON_REPEATER_CARBINE_label = "Carbine Repeater",
+        WEAPON_RIFLE_VARMINT_label = "Varmint Rifle"
+    } 
+elseif Language == "Francais" then
+    Config.Translate = {
+        invFullWeapon = "Vous n'avez pas de place pour une autre arme",
+        invFullItems = "Vous n'avez plus de place pour ",
+        youGot = "Vous obtenez ",
+        nugget = " pépites",
+        water = "un litre d'eau",
+        ammorepeaternormal = "des munitions pour fusil à répétition",
+        ammoriflenormal = "des munitions pour fusil",
+        WEAPON_REVOLVER_CATTLEMAN_label = "un révolver Cattleman",
+        WEAPON_REPEATER_CARBINE_label = "une carabine à répétition",
+        WEAPON_RIFLE_VARMINT_label = "un fusil à petit gibier"
+    }  # isn't there a table somewhere, where I could just pull the translation of items ?
+end
